12d18abbd1ff00d8da4192d75513d72204d5c3e9
subyster <arthur.amaral1@gmail.com>
2021-05-04 15:41:48 -0300

    Auth com SSR

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
+
 import { useAuth } from "../contexts/AuthContext"
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -15,3 +18,12 @@ export default function Dashboard() {
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  return {
+    props: {}
+  }
+})
---
+++ src/utils/withSSRAuth.ts
@@ -0,0 +1,36 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next"
+import { destroyCookie, parseCookies } from "nookies"
+import { AuthTokenError } from "../errors/AuthTokenError"
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx)
+
+    // retorna pra login se não estiver em sessão
+    if (!cookies['nextauth.token']) {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
+
+    // retorna se houver problema com o token
+    try {
+      return await fn(ctx)
+    } catch (err) {
+      if (err instanceof AuthTokenError) {
+        destroyCookie(ctx, 'nextauth.token')
+        destroyCookie(ctx, 'nextauth.refreshToken')
+
+        return {
+          redirect: {
+            destination: '/',
+            permanent: false,
+          }
+        }
+      }
+    }
+  }
+}
