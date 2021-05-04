e4fb67f3e6072bf15ec7aa6ea2d9ca50a20d2bed
subyster <arthur.amaral1@gmail.com>
2021-05-04 10:39:44 -0300

    Autenticação com JWT

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext"
+import { api } from "../services/api";
+
+export default function Dashboard() {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    api.get('/me')
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }, [])
+
+  return (
+    <h1>Dashboard: {user?.email}</h1>
+  )
+}
